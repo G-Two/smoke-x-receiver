93734b9b0084912d537f384a8556a446e4f44585
sam-dot-l <75334184+sam-dot-l@users.noreply.github.com>
2022-01-06 20:06:37 -0500

    Reduce vendor-chunk.js production distribution size  (#1)
    * Removes importing entire Bootstrap Vue library and instead only loods the needed plugins.
    
    * Make prettier.

--- web_ui/src/main.js
+++ web_ui/src/main.js
@@ -1,11 +1,28 @@
 import Vue from "vue";
 import App from "./App.vue";
-import BootstrapVue from "bootstrap-vue";
+import {
+  TooltipPlugin,
+  FormPlugin,
+  LayoutPlugin,
+  ButtonPlugin,
+  FormInputPlugin,
+  FormCheckboxPlugin,
+  FormRadioPlugin,
+  FormGroupPlugin,
+} from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import router from "./router";
 
-Vue.use(BootstrapVue);
+// For tree shaking purposes
+Vue.use(TooltipPlugin);
+Vue.use(FormPlugin);
+Vue.use(LayoutPlugin);
+Vue.use(ButtonPlugin);
+Vue.use(FormCheckboxPlugin);
+Vue.use(FormInputPlugin);
+Vue.use(FormGroupPlugin);
+Vue.use(FormRadioPlugin);
 
 Vue.config.productionTip = false;
 
