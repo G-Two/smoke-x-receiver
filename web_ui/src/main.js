19a01da8df041c246506017a9c8032fc8ab55cfc
sam-dot-l <75334184+sam-dot-l@users.noreply.github.com>
2022-01-06 19:45:24 -0500

    Make prettier.

--- web_ui/src/main.js
+++ web_ui/src/main.js
@@ -1,29 +1,28 @@
 import Vue from "vue";
 import App from "./App.vue";
 import {
-    TooltipPlugin,
-    FormPlugin,
-    LayoutPlugin,
-    ButtonPlugin,
-    FormInputPlugin,
-    FormCheckboxPlugin,
-    FormRadioPlugin,
-    FormGroupPlugin
-} from "bootstrap-vue"
+  TooltipPlugin,
+  FormPlugin,
+  LayoutPlugin,
+  ButtonPlugin,
+  FormInputPlugin,
+  FormCheckboxPlugin,
+  FormRadioPlugin,
+  FormGroupPlugin,
+} from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import router from "./router";
 
 // For tree shaking purposes
-Vue.use(TooltipPlugin)
-Vue.use(FormPlugin)
-Vue.use(LayoutPlugin)
-Vue.use(ButtonPlugin)
-Vue.use(FormCheckboxPlugin)
-Vue.use(FormInputPlugin)
-Vue.use(FormGroupPlugin)
-Vue.use(FormRadioPlugin)
-
+Vue.use(TooltipPlugin);
+Vue.use(FormPlugin);
+Vue.use(LayoutPlugin);
+Vue.use(ButtonPlugin);
+Vue.use(FormCheckboxPlugin);
+Vue.use(FormInputPlugin);
+Vue.use(FormGroupPlugin);
+Vue.use(FormRadioPlugin);
 
 Vue.config.productionTip = false;
 
